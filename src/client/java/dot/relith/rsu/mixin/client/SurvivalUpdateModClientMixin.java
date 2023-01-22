64e1605c074273c4fc741132628ed6d2d0a8ac24
dotRelith <digimonfusion280@gmail.com>
2023-01-22 03:04:34 -0300

    initial files + todo

---
+++ src/client/java/dot/relith/rsu/mixin/client/SurvivalUpdateModClientMixin.java
@@ -0,0 +1,15 @@
+package dot.relith.rsu.mixin.client;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class SurvivalUpdateModClientMixin {
+	@Inject(at = @At("HEAD"), method = "run")
+	private void run(CallbackInfo info) {
+		// This code is injected into the start of MinecraftClient.run()V
+	}
+}
---
+++ src/main/java/dot/relith/rsu/SurvivalUpdateModDataGenerator.java
@@ -0,0 +1,11 @@
+package dot.relith.rsu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SurvivalUpdateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
